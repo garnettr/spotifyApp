f36131f4386125497a26a3b531585d3af3741074
Ian Garnett <designpxls@gmail.com>
2021-04-07 07:36:53 -0400

    Updates

---
+++ precache-manifest.76c636e958c3165a58d75a79b5bba9e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ebd3e60e966b80fffd5",
+    "url": "/spotifyApp/static/css/main.4b34c3d3.chunk.css"
+  },
+  {
+    "revision": "4ebd3e60e966b80fffd5",
+    "url": "/spotifyApp/static/js/main.4d5c9526.chunk.js"
+  },
+  {
+    "revision": "76660c8ae5d0cf98481e",
+    "url": "/spotifyApp/static/js/runtime~main.be89c345.js"
+  },
+  {
+    "revision": "dec0349eea1d7c60db6d",
+    "url": "/spotifyApp/static/js/2.67f276a5.chunk.js"
+  },
+  {
+    "revision": "69e5ce25a250b95c211f40bcb16d4a7d",
+    "url": "/spotifyApp/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1de7b7e5e3038b4228e5bdd3175aa61a.js"
+  "/spotifyApp/precache-manifest.76c636e958c3165a58d75a79b5bba9e2.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/spotifyApp/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
